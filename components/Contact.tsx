acc3c6ff942f4b99d7486d35abe4a44e1f0c21bc
maurice-cubicit <maurice@cubicit.co.za>
2023-01-08 10:54:39 +0200

    fix: apostrophe

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className='bg-green-400 py-20'>
         <div className='w-9/12 m-auto'>
-            <h1 className='text-center text-6xl text-white font-semibold'>LET'S GET IN TOUCH</h1>
+            <h1 className='text-center text-6xl text-white font-semibold'>LET&#39;S GET IN TOUCH</h1>
 
         </div>
     </div>
